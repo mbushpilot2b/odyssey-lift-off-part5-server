a90293a662715d575cdc23f4b1632a24a8298865
Daniel Glassford <mbushpilot2b@gmail.com>
2022-11-27 07:22:56 -0500

    Fix Bug

--- src/resolvers.js
+++ src/resolvers.js
@@ -45,10 +45,10 @@ const resolvers = {
       return dataSources.trackAPI.getTrackModules(id);
     },
     
-    duractionInSeconds: ({ length }) => length,
+    durationInSeconds: ({ length }) => length,
   },
   Module: {
-    duractionInSeconds: ({ length }) => length,
+    durationInSeconds: ({ length }) => length,
   },
 };
 
